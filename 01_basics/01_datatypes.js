41f1ddaf688d6e45c6d7ab74d8a414ac1a5cb42c
SantoshAutomationCode <45512851+santoshmolkire@users.noreply.github.com>
2023-12-08 06:39:21 +0000

    var and datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newr version
+
+//alert(3+3);//we r using nodejs not browser
+
+console.log(3+3);
+console.log(3+
+    3);//code readability that should be high
+
+    console.log("santy");
+
+    let age=30;//Number
+    let name="";//String
+    let isLoggedIn=true;//boolean 
+    /* datatypes */
+    //Number->2 to power 53
+    //BigInt
+    //String=""
+    //boolean->true or false
+    //null-> standalone value
+    //undefined-> not defined
+    //symbol-> unique
+
+    console.log(typeof age);//Number
+    console.log(typeof name);//String
+    console.log(typeof null);//object
+    console.log(typeof undefined);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accounId=73637;
+let email="ss@gmail.com";
+var name="santy";
+city="bengaluru";
+let country;
+//accounId=47849;//not allowed to assign value to constant
+
+email="ssm@hotmail.com";
+name="ssm";
+city="pune";
+
+console.log(accounId);
+console.log(email);
+console.log(name);
+console.log(city);
+
+console.table([accounId,email,name,city,country]);
+
+/* not to use var bcoz of issue in block and functions*/
+
