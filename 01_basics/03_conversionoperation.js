a0dc9c0a0ce469a02326406636f564ec12b6af9e
SantoshAutomationCode <45512851+santoshmolkire@users.noreply.github.com>
2023-12-08 07:26:10 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,13 @@
+//let score="33";//String
+//let score="33abc";
+let score=true;
+
+console.log(typeof score);
+
+let valueNumber=new Number(score);
+console.log(valueNumber);//33 before adding abc after NaN not a Number
+
+console.log(typeof(valueNumber));//Object
+
+
+
